746b4ab238d8299e461d4c5fd3e82bfeccf71b74
codewithtayyabashahid <167111800+codewithtayyabashahid@users.noreply.github.com>
2025-01-21 22:06:24 -0800

    my-hackathon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import "slick-carousel/slick/slick.css";
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,15 +16,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen font-sans antialiased bgDesign",
+          inter.className
+        )}
       >
-        {children}
+        <Layout>
+          <Navbar />
+          {children}
+          <Footer />
+        </Layout>
       </body>
     </html>
   );
